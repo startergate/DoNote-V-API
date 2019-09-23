e4a232bfb07239ff0863b188c517570fd9842948
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-23 22:50:37 +0900

    Create universalModules.js

---
+++ src/modules/universalModules.js
@@ -0,0 +1,11 @@
+exports.randomString = (length) => {
+  const character = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randomStr = '';
+  let loopNum = length;
+  while (loopNum) {
+    randomStr += character[Math.floor(Math.random() * character.length)];
+    loopNum -= 1;
+  }
+
+  return randomStr;
+};
