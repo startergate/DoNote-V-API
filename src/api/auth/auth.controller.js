3afe4d7524c9c20e2a628a3ab82aa9dc622f2a8f
startergate <startergate36@gmail.com>
2020-02-28 20:03:20 +0900

    [UPDATE] make actual login flow

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -1,3 +1,20 @@
-exports.loginFlow = ctx => {
+const { user, note } = require('models');
 
+exports.loginFlow = ctx => {
+  const pid = ctx.request.query.pid;
+  
+  user.findOrCreate({
+    where: {
+      pid: pid
+    },
+    defaults: {
+      pid: pid,
+      recentSessid: ctx.request.query.sessid
+    }
+  });
+  
+  note.tableName = `notedb_${pid}`;
+  note.sync();
+  
+  ctx.redirect('/note');
 };
